53b43961c82a62e654d48e5be0f0c0bdb0eff2aa
Berta Devant <bertadeva@gmail.com>
2015-11-19 16:25:37 -0500

    Copied Project 1 and started modifying

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -7,32 +7,60 @@
 //
 
 import UIKit
+import Social
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    //@IBOutlet weak var detailDescriptionLabel: UILabel! //IBOUtlet connects Interface Builder, weak because View owns it not in memory
 
+    @IBOutlet weak var detailImageView: UIImageView!
 
-    var detailItem: AnyObject? {
+    var detailItem: String? { //no need to have object, we know is array of Strings
         didSet {
-            // Update the view.
-            self.configureView()
+            // Update the view. If detailItem is change we configure the view
+            configureView()
         }
     }
 
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
+        if let detail = self.detailItem { //unwrapping and checking that detailItem has value
+            if let imageView = self.detailImageView { //unwrapping and checking if imageview has value
+                imageView.image = UIImage(named:detail) //UIImage is data type to load images, named passes name of image 
+                                                        //by putting detail we pass the image selected by user and pass through detailItem
             }
         }
     }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: "sharedTapped")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated) //super calls on parent method
+        navigationController?.hidesBarsOnTap = true
+        navigationItem.title = detailItem
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    func sharedTapped()
+    {
+        let vc = UIActivityViewController(activityItems: [detailImageView.image!], applicationActivities: [])
+        presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    func socialFB ()
+    {
+        let vc = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+        vc.setInitialText("Look at this great picture!")
     }
 
     override func didReceiveMemoryWarning() {
--- Project1/MasterViewController.swift
+++ Project1/MasterViewController.swift
@@ -6,24 +6,32 @@
 //  Copyright © 2015 Berta Devant. All rights reserved.
 //
 
+//Trinity: 
+//Model - something that describes the data you are working with 
+//View - user interface of the app
+//Controller - code that sends model data to and from the view
+
 import UIKit
 
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
+    var objects = [String]() //changed [AnyObject] to String because we know we are only using Strings on this app
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        let fm = NSFileManager.defaultManager() //look for files
+        //where to look for files, we add ! because we are sure it will return a string and if we didnt add it we would have to add an unwrapping method for possible optionals
+        let path = NSBundle.mainBundle().resourcePath!
+        //items with look and where returns STRING on FILENAMES try! because we are certain the code will not fail, we do not need try catch methods then
+        let items = try! fm.contentsOfDirectoryAtPath(path)
+        
+        for item in items { //treat items as a series of text strings, then pull out each one of those text strings, give it the name item, then run the following code block
+            if item.hasPrefix("nssl")
+            {
+                objects.append(item) //adds matching filename to a list called objects that xCode creates
+            }
         }
     }
 
@@ -36,21 +44,28 @@ class MasterViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    /*Deleting this
 
     func insertNewObject(sender: AnyObject) {
         objects.insert(NSDate(), atIndex: 0)
         let indexPath = NSIndexPath(forRow: 0, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
+    } */
 
     // MARK: - Segues
 
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) { //prepareForSegue allows you to change a view before loading it 
+                                                                                //or add customization befor ethe new controller is shown
         if segue.identifier == "showDetail" {
-            if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+            if let indexPath = self.tableView.indexPathForSelectedRow { //we tell indexPAth to be what user selects
+                /* replaced
+                let object = objects[indexPath.row] //as! NSDate
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                controller.detailItem = object */
+                let navigationController = segue.destinationViewController as! UINavigationController //it is telling it the destination ocntroller after segue is a Navigatrion controller
+                let controller = navigationController.topViewController as! DetailViewController //we override swift and tell hime our TopViewController is definetely DetalViewControl;ler
+                controller.detailItem = objects[indexPath.row] //detailItem is the object of the selected row
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
@@ -70,8 +85,8 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row] //as! NSDate
+        cell.textLabel!.text = object //.description we took this out because we know all the objects will be Strings, no need for description
         return cell
     }
 
@@ -79,7 +94,7 @@ class MasterViewController: UITableViewController {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
+/* Deleting this
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             objects.removeAtIndex(indexPath.row)
@@ -87,7 +102,7 @@ class MasterViewController: UITableViewController {
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
-    }
+    } */
 
 
 }
